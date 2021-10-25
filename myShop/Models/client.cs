c1e8acdbd8dee14bc52dbf1e9bc6a82868e42727
Calebe de Araújo Stodulski <00209875283@grupo.rovema>
2021-10-25 11:27:08 -0400

    created product and client classes

---
+++ myShop/Models/client.cs
@@ -0,0 +1,8 @@
+namespace client.models
+{
+    public class client
+    {
+        int id_client {get; set;}
+        string name {get; set;}
+    }
+}
---
+++ myShop/Models/product.cs
@@ -0,0 +1,10 @@
+namespace product.models
+{
+    public class product
+    {
+        public int Id {get; set;}
+        public string description {get; set;}
+        public float price {get; set;}
+        int stock_quantity {get; set;}
+    }
+}
